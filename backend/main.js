03be0166e9a7fe3f421865f7cb046cb660118bc3
Arka Saha <i.am.arka.saha@gmail.com>
2024-12-09 18:18:26 +0530

    Update main.js

--- backend/main.js
+++ backend/main.js
@@ -33,9 +33,9 @@ const girls = [
 
 next_btn.onclick= ()=>
 {
-    next_btn.disabled = true;
+    // next_btn.disabled = true;
     setTimeout(()=>
-    {next_btn.disabled=false}, 500);
+    {next_btn.focus()}, 500);
     console.log(bois[count], girls[count])
     img_boi.src = `imgs/${bois[count]}`
     img_girl.src = `imgs/${girls[count]}`
