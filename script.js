e113a19a4a075880a2a7d46d72ec679c1d5a09c3
Arka Saha <i.am.arka.saha@gmail.com>
2025-01-24 22:52:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,13 +5,37 @@ let txt = document.getElementsByClassName("txt")[0];
 const cards = document.querySelectorAll('.card');
 
 cards.forEach(card => {
+    card.draggable = true;
 
-    card.ontouchmove = (event) =>
+    const func = (e) =>
+        {
+            console.log('helo');
+            // e.preventDefault();
+            card.draggable = true;
+            card.style.transition = 'none'; // Disable smooth transitions while dragging
+            card.style.zIndex = 1000; // Bring the card to the front
+
+        
+        }
+    card.ondrag = (event) =>
     {
+        console.log('ss');
+        
 
-        var x = event.touches[0].clientX;
-        var y = event.touches[0].clientY;
-        txt.textContent = `x: ${x} y: ${y}`;
+        var x = event.clientX || event.touches[0].clientX;
+        var y = event.clientY || event.touches[0].clientY;
+        txt.textContent = x;
+        card.style.marginLeft = `${x}px`;
+        card.style.marginTop = `${y}px`;
+
     }
-    
+
+    card.onclick = () =>
+    {
+        txt.textContent = "ar";
+    }
+    // card.ondra
+    card.addEventListener('mousedown', func);
+    card.addEventListener('touchstart', func);
+
 });
