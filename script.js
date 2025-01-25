ebe3ee5d0b10149dc6498e95d66230cac2b6695b
Arka Saha <i.am.arka.saha@gmail.com>
2025-01-25 23:17:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,32 @@
-let txt = document.getElementsByClassName("txt")[0];
+const arr_txt = [
+    "With you even the simplest tuktuk auto ride, feels like a ride in the most expensive sports car.",
+    "Even the hair band you gave me, it feels like wearing the most beautiful, expensive Rolex watch.",
+    "That day when you paid for me, when i was broke hehe, i felt like the richest man in this world.",
+    "With you, your beautiful voice notes feels like the most pleasant and soothing voice i can ever hear.",
+    "With you, the fotos ya send me, feels like my eyes have seen the entirity of the world in one frame.",
+    "With you even our silly chats, being childsh, being little kids again, feels like you are right here holding my hands, telling me I am safe with you always.",
+    "With you our video koll vlogs feels like me airjailey ya and we going through sand on the beach, or seeing the beautiful views from a mountain.",
+    "With you, whenever ya eepy koll me and i comfort ya to eep, it feels like ya slowly snuggleyy with me till ya comfy and i gently kiss you goodnitey and we eeppy.",
+    "With you, whenever ya compliment me, call me handsome hehe, i feel handsome fr shhh.",
+    "With you na, everything is just perfect ✨️"
+];
+  
+let btn = document.querySelector(".btn");
+let txt1 = document.querySelector(".txt1");
+let txt2 = document.querySelector(".txt2");
 
-// txt.textContent ="h11e";
-
-const cards = document.querySelectorAll('.card');
-
-cards.forEach(card => {
-    card.draggable = true;
-
-    const func = (e) =>
-        {
-            console.log('helo');
-            // e.preventDefault();
-            card.draggable = true;
-            card.style.transition = 'none'; // Disable smooth transitions while dragging
-            card.style.zIndex = 1000; // Bring the card to the front
-
-        
-        }
-    card.touchmove = (event) =>
+let c= 0;
+const l = arr_txt.length;
+btn.onclick = () =>
+{
+    
+    if(c >=l)
     {
-        console.log('ss');
-        
-
-        var x = event.clientX || event.touches[0].clientX;
-        var y = event.clientY || event.touches[0].clientY;
-        txt.textContent = x;
-        card.style.marginLeft = `${x}px`;
-        card.style.marginTop = `${y}px`;
-
+        txt1.textContent = c;
     }
-
-    card.onclick = () =>
+    else
     {
-        txt.textContent = "arkaa";
+        txt1.textContent = arr_txt.at(c++);
     }
-    // card.ondra
-    card.addEventListener('mousedown', func);
-    card.addEventListener('touchstart', func);
-
-});
+    
+}
