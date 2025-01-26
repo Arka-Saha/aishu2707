c87bd5aff6916e215a018e520425ad90f2309bf7
Arka Saha <i.am.arka.saha@gmail.com>
2025-01-26 19:23:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,23 +10,113 @@ const arr_txt = [
     "With you, whenever ya compliment me, call me handsome hehe, i feel handsome fr shhh.",
     "With you na, everything is just perfect ✨️"
 ];
-  
+const loveMessageArray = [
+    "I loveee you naa mah sweetheart",
+    "Ya always mahh little baby girl naa",
+    "I am never ever going anywhere na babyy chehehe",
+    "I would never fall in love again until I found you",
+    "Yk I always really wanna jus go there to ya when we have any misunderstandings na",
+    "And hold youu close to me, kissing away all your tears and pain",
+    "I wanna hold your hands and go on peaceful nite walkie with ya",
+    "I wanna huggeywuggy air jail yaa till everything becomes goodie",
+    "I lovee youu mah baby, and I will always be in love with you na sweetheart",
+    "You are the besstesttt hooman i can ebah habb chehehe mah lovee eueuehhee"
+];
+
+// const arr_txt = ["arka", "test"];
 let btn = document.querySelector(".btn");
 let txt1 = document.querySelector(".txt1");
 let txt2 = document.querySelector(".txt2");
+let first = document.querySelector(".first-div");
+let first_txt = document.querySelector(".first-txt");
 
 let c= 0;
 const l = arr_txt.length;
+let d = 0;
+
+const interv = setInterval(() => {
+    // const first_arr = ['test'];
+    const first_arr = ["I", "love", "you", "sooooooo", "much", "na", "yk", "with", "you", "na"];
+
+    const fl = first_arr.length;
+
+    first_txt.textContent = first_arr.at(d);
+    
+    // d++;
+    if(d===fl)
+    {
+        clearInterval(interv);
+        first_screen();
+    }
+    d++;
+}, 1000);
+
+function first_screen()
+{
+    first.style.opacity = 0;
+    first.style.visibility = "collapse";
+}
+let a = 0;
 btn.onclick = () =>
 {
-    
+    // btn.style.backgroundColor = "red";
     if(c >=l)
     {
-        txt1.textContent = c;
+        // txt1.textContent = c;
+        first.style.visibility = "visible";
+        first.style.opacity = "95%";
+        first_txt.textContent = "Perfect ✨";
+        const sec_interv = setInterval(() => {
+            const apologyArray = ["you", "are", "perfect", "na", "sweetheart.", "I", "am", "sorrrey", "for", "all", "the misunderstandings", "we had na", "🥺", "can", "we", "have", "us", "back", "na", "sweetheart, please.", "🥺"];
+            const al = apologyArray.length;
+            first_txt.textContent = apologyArray.at(a);
+            if(a===12)
+            {
+                first_txt.style.color = "wheat";
+            }
+            if(a>=al)
+            {
+                clearInterval(sec_interv);
+                first_screen2();
+            }
+            a++;
+        }, 1000);
     }
     else
     {
-        txt1.textContent = arr_txt.at(c++);
+        txt1.textContent = arr_txt.at(c);
+        txt2.textContent = loveMessageArray.at(c);
+        c++;
     }
-    
+
+    // btn.style.backgroundColor = "white";
+}
+
+let x = 0;
+function first_screen2()
+{
+    const valentine = ["a-and 👀",
+        "sweetheart na ❤️", 
+        "Will", "you", "be", "my", "valentine", "Will you be my valentine ? 🥺👉👈","Will you be my valentine ? 🥺👉👈"
+    ];
+
+
+    let vl = valentine.length;
+    const third_interv = setInterval(() => {
+        first_txt.textContent = valentine.at(x++);
+        if(x == vl+1)
+        {
+            setTimeout(() => {
+                window.open("foto.jpeg");
+            }, 2000);
+            first.style.opacity = 0;
+            first.style.visibility = "collapse"; 
+            clearInterval(third_interv);
+            x=0;
+            d=0;
+            c=0;
+            a=0;
+        }
+        
+    }, 1000);
 }
